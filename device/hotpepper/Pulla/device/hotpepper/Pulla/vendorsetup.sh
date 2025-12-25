ba28f6feaec777d40e1b412ace04dca458ef6122
fedoraman83 <jaychandler@hotmail.com>
2025-12-25 16:35:22 -0500

    undo

--- device/hotpepper/Pulla/device/hotpepper/Pulla/vendorsetup.sh
+++
@@ -1,9 +0,0 @@
-#
-# Copyright (C) 2025 The Android Open Source Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-add_lunch_combo omni_Pulla-user
-add_lunch_combo omni_Pulla-userdebug
-add_lunch_combo omni_Pulla-eng
-
