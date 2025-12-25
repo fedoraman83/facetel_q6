4fe3f006256f7310486a7a4b56d826a624b6ecb2
fedoraman83 <jaychandler@hotmail.com>
2025-12-25 18:14:34 -0500

    Update omni_Pulla.mk

--- device/hotpepper/Pulla/omni_Pulla.mk
+++ device/hotpepper/Pulla/omni_Pulla.mk
@@ -15,7 +15,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit from Pulla device
-$(call inherit-product, $(LOCAL_PATH)/device.mk)
+$(call inherit-product, device.mk)
 
 PRODUCT_DEVICE := Pulla
 PRODUCT_NAME := omni_Pulla
