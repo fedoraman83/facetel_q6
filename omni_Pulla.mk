7ce2b41c272ff648b621131cb6c0aa8710acf843
fedoraman83 <jaychandler@hotmail.com>
2025-12-26 13:03:33 -0500

    Update omni_Pulla.mk

--- omni_Pulla.mk
+++ omni_Pulla.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+#$(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit from Pulla device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
